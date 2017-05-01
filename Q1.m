c836d2f85b318ffc186e57a7f2ac9fec0269f6f8
Yucheng Lin <Pike96@users.noreply.github.com>
2017-04-30 18:12:26 -0700

    Update Q1.m

--- Q1.m
+++ Q1.m
@@ -1,7 +1,7 @@
 %% Monte Carlo integration (a)
 disp(['Monte Carlo integration:']);
 clear; close all;
-n = 1e6;
+n = 1e3;
 
 ga =@(x)exp(sum(5.*abs(x-5)));
 Xa = ga(rand(2,n));
@@ -14,7 +14,7 @@ disp(['Estimate: ',num2str(est_a),'	Variance: ',num2str(var_a)]);
 
 %% (b)
 clear; close all;
-n = 1e6;
+n = 1e3;
 
 gb = @(x)cos(pi + sum(5.*x));
 Xb = gb(2.*rand(2,n)-1);
@@ -28,11 +28,11 @@ disp(['Estimate: ',num2str(est_b),'	Variance: ',num2str(var_b)]);
 %% Stratified sampling (a)
 disp(['Stratified sampling:']);
 clear; close all;
-n = 1e6;
+n = 1e4;
 
 ga =@(x)exp(sum(5.*abs(x-5)));
 Xa = ga(rand(2,n));
-K = 20; Nij = n/K^2; % Stratified
+K = 10; Nij = n/K^2; % Stratified
 for i = 1:K
     for j = 1:K
         XS = ga([i-1+rand(1,Nij);j-1+rand(1,Nij)]/K);
@@ -45,11 +45,11 @@ disp(['Estimate: ',num2str(est_a), '	Variance: ',num2str(var_a)]);
 
 %% (b)
 clear; close all;
-n = 1e6;
+n = 1e3;
 
 gb = @(x)cos(pi + sum(5.*x));
 Xb = gb(2.*rand(2,n)-1);
-K = 20; Nij = n/K^2; % Stratified
+K = 10; Nij = n/K^2; % Stratified
 for i = 1:K
     for j = 1:K
         XS = gb([i-1+rand(1,Nij);j-1+rand(1,Nij)]/K);
@@ -63,15 +63,23 @@ disp(['Estimate: ',num2str(est_b),'	Variance: ',num2str(var_b)]);
 %% Importance sampling (a)
 disp(['Importance sampling:']);
 clear; close all;
-n = 1e6;
+n = 1e3;
 
-%w = @(x)1./mvnpdf(x',[5 5])';
-%w = @(x)1./(1/sqrt(2*pi).*exp(-(x(1,:)-5).^2/2-(x(2,:)-5).^2/2));
-w = @(x)1./(1/sqrt(2*pi).*exp(-(x-5).^2/2));
-f = @(x)exp(5.*abs(x-5));
-%f = @(x)exp(sum(5.*abs(x-5)));
-X = normrnd(5,1,2,n);
-T = w(X(1,:)).*f(X(1,:)).*w(X(2,:)).*f(X(2,:));
-%T = (e-1)^2*exp(sum(5.*abs(X-5)).^2-sum(X));
+U = rand(2,n);
+e = exp(1); X = log(1+(e-1)*U);
+T = (e-1)^2*exp(50-6*sum(X));
 est_a = mean(T);
 var_a = 2*std(T)/sqrt(n);
+disp(['Estimate: ',num2str(est_a), '	Variance: ',num2str(var_a)]);
+
+%% (B)
+clear; close all;
+n = 1e3;
+
+U = rand(2,n);
+e = exp(1); X = log(1+(e-1)*U);
+T = -(e-1)^2/2*(exp(i*5*sum(X)-sum(X))+exp(-i*5*sum(X)-sum(X)));
+est_b = mean(T);
+var_b = 2*std(T)/sqrt(n);
+
+disp(['Estimate: ',num2str(est_b),'	Variance: ',num2str(var_b)]);
