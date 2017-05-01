5857c750cb8ecace243e017eeb0b47bc99b13f30
Yucheng Lin <Pike96@users.noreply.github.com>
2017-05-01 00:35:23 -0700

    Update Q2.m

--- Q2.m
+++ Q2.m
@@ -1,9 +1,15 @@
 %% a
 clear; close all;
+% Simple MC
+n = 1e7; X2 = -diag(1./[1:3])*log(rand(3,n));
+S = sum(X2); H = S.*(S>15)/mean(S>15);
+disp(['Simple MC (a):']);
+disp(['Estimate: ',num2str(mean(H)),...
+    '	Variance: ',num2str(2*std(H)/sqrt(n))]);
 % Gibbs Sampling
-N = 1e5; 
-X = 3*ones(1,3);
-for i = 1:N
+n = 1e5; 
+X = ones(1,3)*3;
+for i = 1:n
     j = ceil(3*rand);
     S = sum(X) - X(j);
     X(j) = max(15-S - log(rand)/j,0);
@@ -11,20 +17,20 @@ for i = 1:N
 end
 disp(['Gibbs Sampling (a):']);
 disp(['Estimate: ',num2str(mean(H)),...
-    '	Variance: ',num2str(2*std(H)/sqrt(N))]);
-% Simple MC
-N = 1e7; X2 = -diag(1./[1:3])*log(rand(3,N));
-S = sum(X2); H = S.*(S>15)/mean(S>15);
-disp(['Simple MC (a):']);
-disp(['Estimate: ',num2str(mean(H)),...
-    '	Variance: ',num2str(2*std(H)/sqrt(N))]);
+    '	Variance: ',num2str(2*std(H)/sqrt(n))]);
 
 %% b
 clear; close all;
+% Simple MC
+n = 1e7; X2 = -diag(1./[1:3])*log(rand(3,n));
+S = sum(X2); H = S.*(S<1)/mean(S<1);
+disp(['Simple MC (b):']);
+disp(['Estimate: ',num2str(mean(H)),...
+    '	Variance: ',num2str(2*std(H)/sqrt(n))]);
 % Gibbs Sampling
-N = 1e5; 
-X = 0.2*ones(1,3);
-for i = 1:N
+n = 1e5; 
+X = ones(1,3)*3;
+for i = 1:n
     j = ceil(3*rand);
     S = sum(X) - X(j);
     X(j) = max(1-S + log(rand)/j,0);
@@ -32,10 +38,4 @@ for i = 1:N
 end
 disp(['Gibbs Sampling (b):']);
 disp(['Estimate: ',num2str(mean(H)),...
-    '	Variance: ',num2str(2*std(H)/sqrt(N))]);
-% Simple MC
-N = 1e7; X2 = -diag(1./[1:3])*log(rand(3,N));
-S = sum(X2); H = S.*(S<1)/mean(S<1);
-disp(['Simple MC (b):']);
-disp(['Estimate: ',num2str(mean(H)),...
-    '	Variance: ',num2str(2*std(H)/sqrt(N))]);
+    '	Variance: ',num2str(2*std(H)/sqrt(n))]);
