a690d48647783b73d015531a65de0cff8386b644
Yucheng Lin <Pike96@users.noreply.github.com>
2017-05-02 21:46:18 -0700

    Update Q4.m

--- Q4.m
+++ Q4.m
@@ -2,17 +2,13 @@ clear; close all;
 fileID = fopen('uscap_xy.txt');
 data = textscan(fileID, '%f %f');
 fclose(fileID);
-Axis(:,1) = data{1};
-Axis(:,2) = data{2};
+Axis(:,1) = data{1}; Axis(:,2) = data{2};
 start = Axis(5,:); % Start from Sacramento
 % Delete 2 extra and start
-Axis(2,:) = [];
-Axis(4,:) = [];
-Axis(9,:) = [];
+Axis(2,:) = []; Axis(4,:) = []; Axis(9,:) = [];
 n = 47;
 for k = 1:1e4
-    T = 10000;
-    C = randperm(n);
+    T = 10000; C = randperm(n);
     TD(k) = d(start,n,C,Axis);
     B = 1; time(k) = 0;
     while B ~= 0, C2 = C;
@@ -21,8 +17,7 @@ for k = 1:1e4
         TD2 = d(start,n,C2,Axis);
         B = exp((TD(k)-TD2)/T);
         if rand < B, C = C2; TD(k) = TD2; end
-        T = .99*T;
-        time(k) = time(k)+1;
+        T = .99*T; time(k) = time(k)+1;
     end
     if TD(k) < 19000, break; end;
 end
@@ -32,18 +27,12 @@ scatter(start(1),start(2),'x')
 xlabel('x'); ylabel('y');
 hold on
 scatter(Axis(:,1),Axis(:,2),'x')
-hold on
 plot([start(1),Axis(C(1),1)],[start(2),Axis(C(1),2)])
-hold on
 for i=2:length(C)
-    x0=Axis(C(i-1),1);
-    y0=Axis(C(i-1),2);
-    x1=Axis(C(i),1);
-    y1=Axis(C(i),2);
-    xx=[x0,x1];
-    yy=[y0,y1];
+    x0=Axis(C(i-1),1); y0=Axis(C(i-1),2);
+    x1=Axis(C(i),1); y1=Axis(C(i),2);
+    xx=[x0,x1]; yy=[y0,y1];
     plot(xx,yy)
-    hold on
 end
 
 figure;
